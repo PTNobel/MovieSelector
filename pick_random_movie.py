83095853be4aa246e5a9cc79bfc9b7ccb19edb86
Parth Nobel <parthnobel@berkeley.edu>
2017-09-11 11:59:49 -0700

    Rewrites pick_random_movie.py and adds a .gitignore

--- pick_random_movie.py
+++ pick_random_movie.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python3
 
-from typing import List, Optional
+from typing import List, Optional, Callable
 import sys
 from numpy.random import chisquare
+from random import gauss
 
 # def chisquare(df: int, shape: Any=...) -> float: ...
 
-NUMBER_OF_CHISQUARE_DRAWS_TO_ATTEMPT: int = 50
+GetRandomIndex = Callable[[int], int]
+
+NUMBER_OF_DRAWS_TO_ATTEMPT: int = 50
 DISABLE_READ_IN_MOVIES: bool = False
+ABS_Z_GENERATOR: GetRandomIndex = lambda length: int(abs(gauss(0, length/3)))
+CHISQUARE_GENERATOR: GetRandomIndex = lambda length: int(chisquare(length//2))
+RANDOM_NUMBER_GENERATION_RULE: GetRandomIndex = ABS_Z_GENERATOR
+
+
+def getRandomIndex(length: int) -> int:
+    # Choose a random number according to the rules defined in
+    # RANDOM_NUMBER_GENERATION_RULE
+    randomNumber: int = RANDOM_NUMBER_GENERATION_RULE(length)
+
+    # Let's keep track of how many times we've drawn numbers.
+    tries: int = 0
+    # If we have two great an index, let's try again.
+    while randomNumber >= length:
+        # Draw a new number...
+        randomNumber = RANDOM_NUMBER_GENERATION_RULE(length)
+        # We don't want the program to run forever... so let's just throw an
+        # error after NUMBER_OF_DRAWS_TO_ATTEMPT.
+        if tries > NUMBER_OF_DRAWS_TO_ATTEMPT:
+            assert randomNumber >= length, \
+                "Random number generator failed. Check numpy install"
+        tries += 1
+
+    return randomNumber
 
 
 def processargs(): ...
@@ -27,32 +54,14 @@ def readInMovies(fileName: Optional[str] = None) -> List[str]:
 
 
 def main(args: List[str]) -> None:
-    """After substaintial discussion a number drawn from the chi-square
-    distribution with df = len(movies)/2 was chosen as the means of picking
-    a movie to watch. This is biased tolds older entries on the too-watch
-    list. This method chooses a movie according to these rules."""
-    # TODO implement arg processing etc.
+        # TODO implement arg processing etc.
 
     # Get the list of movies
     movies = readInMovies()
 
-    # Choose a random number according to the rules described above.
-    randomNumber: int = int(chisquare(len(movies)//2))
+    index = getRandomIndex(len(movies))
 
-    # Let's keep track of how many times we've drawn numbers.
-    tries: int = 0
-    # If we have two great an index, let's try again.
-    while randomNumber >= len(movies):
-        # Draw a new number...
-        randomNumber = int(chisquare(len(movies)//2))
-        # We don't want the program to run forever... so let's just throw an
-        # error after NUMBER_OF_CHISQUARE_DRAWS_TO_ATTEMPT.
-        if tries > NUMBER_OF_CHISQUARE_DRAWS_TO_ATTEMPT:
-            assert randomNumber >= len(movies), \
-                "Random number generator failed. Check numpy install"
-        tries += 1
-
-    print(movies[int(randomNumber)])
+    print(movies[index])
 
 
 if __name__ == '__main__':
